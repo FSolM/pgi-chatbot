93ae4677d47d9830e45de2fd3664a138e9ee5af9
= <=>
2020-12-30 12:18:50 -0600

    Array Handling Implemented

--- bin/chatbot.ts
+++ bin/chatbot.ts
@@ -6,6 +6,8 @@ export default class ChatBot {
   bot_class: string;
   user_class: string;
 
+  error_message: string;
+
   constructor(container: HTMLElement, library: object) {
     // Sets internal target container for bot usage
     this.container = container;
@@ -13,6 +15,9 @@ export default class ChatBot {
     // Set default classes
     this.bot_class = 'bot-message';
     this.user_class = 'user-message';
+
+    // Set default error message
+    this.error_message = "Sorry, I didn't quite catched that";
 
     // Clears container's HTML
     this.container.innerHTML = '';
@@ -22,19 +27,32 @@ export default class ChatBot {
 
     // Fills internal library
     Object.keys(library).forEach((key) => {
-      key.split('|').forEach((response) => {
-        this.library[response] = library[key];
-      });
+      if (typeof key === 'string') {
+        // Incomming String key
+        key.split('|').forEach((response) => {
+          this.library[response] = library[key];
+        });
+      } else {
+        // Incomming Number key
+        this.library[key] = library[key];
+        // Would be easier to just copy the array instead of looping through all the options again
+        // maybe add a boolean in the constructor, or check the first key, if it's an string then loop, if not then copy
+        // less hasle that way
+      }
     });
   };
 
   // Setters
-  set botClassName(name) {
+  set botClassName(name: string) {
     this.bot_class = name;
   };
 
-  set userClassName(name) {
+  set userClassName(name: string) {
     this.user_class = name;
+  };
+
+  set errorMessage(message: string) {
+    this.error_message = message;
   };
 
   // Methods
@@ -50,8 +68,14 @@ export default class ChatBot {
     message = message.toLowerCase();
     // Clear trailing Spaces
 
-    (message in this.library) ? this.say(this.library[message], 'bot') : this.say("Sorry, I didn't quite catched that", 'bot');
+    (message in this.library) ? this.say(this.library[message], 'bot') : this.say(this.error_message, 'bot');
   };
+
+  respond(message: string, input: number): void {
+    this.say(message, 'user');
+
+    (input in this.library) ? this.say(this.library[input], 'bot') : this.say(this.error_message, 'bot');
+  }
 
   // Helper Methods
   private say(message: string, class_type: string): void {
